4a43b228ae35593ed69f5513dfc59ce781d2faf5
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-03 14:30:40 -0400

    Set up hit points 1 to 5

--- monster.js
+++ monster.js
@@ -1,4 +1,5 @@
 const monster = {
 hitPoints: 30,
 damage: 0,
+isAlive: true,
 };
--- player.js
+++ player.js
@@ -2,4 +2,10 @@ const player = {
 hitPoints: 100,
 damage: 0,
 level: 1,
+isAlive: true,
+fight: function(){
+this.hitPoints = this.hitPoints - Math.ceil((Math.random() * 5));
+console.log(this.hitPoints);
+}
+
 }
