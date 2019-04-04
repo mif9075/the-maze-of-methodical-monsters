1ed4e8b84421d8b3e3e855876b12f43f6417dd82
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-04 03:04:43 -0400

    Monster damage fixed

--- monster.js
+++ monster.js
@@ -1,6 +1,22 @@
 const monster = {
-hitPoints: 30,
-damage: 0,
-isAlive: true,
-lastDamageTaken: 0,
-};
+    hitPoints: 30,
+    damage: 0,
+    isAlive: true,
+    lastDamageTaken: 0,
+
+    calculateRawDamage: function () {
+        return (Math.ceil((Math.random() * 5)) + 2);
+    },
+
+    takeDamage: function () {
+        const rawDamage = (Math.ceil((Math.random() * 5)) + 2);
+        
+        this.lastDamageTaken = rawDamage;
+        this.hitPoints = this.hitPoints - this.lastDamageTaken;
+
+        if (this.hitPoints < 1) {
+            this.isAlive = false;
+        }
+    },
+}
+
--- player.js
+++ player.js
@@ -5,12 +5,21 @@ level: 1,
 isAlive: true,
 lastDamageTaken: 0,
 
-fight: function(){
-this.lastDamageTaken = Math.ceil((Math.random() * 5));
+fight: function(enemy){
+const rawDamage = enemy.calculateRawDamage();
+
+this.lastDamageTaken = rawDamage - this.level;
 this.hitPoints = this.hitPoints - this.lastDamageTaken;
+
+enemy.takeDamage(this.calculateRawDamage());
+
 if (this.hitPoints<1){
     this.isAlive = false;
 }
+},
+
+calculateRawDamage: function(){
+    return Math.ceil((Math.random() * 5));
 },
 
 }
